c284271a3602873985e5c6465eb19bd0af6e7e0a
hypernyan <hypernyan@gmail.com>
2021-03-28 14:41:30 +0300

    small sva fix

--- sim/sva/tcp/tcp_vlg_tx_ctl_sva.sv
+++ sim/sva/tcp/tcp_vlg_tx_ctl_sva.sv
@@ -13,10 +13,10 @@ module tcp_vlg_tx_ctl_sva (
 
   property val_low_if_cts_low;
     @(posedge clk) disable iff (rst) 
-    ($fell(data_cts) |=> $fell(data_val));
+    ($fell(data_cts) |=> !data_val);
   endproperty
  
   ERROR_VAL_HIGH_WHEN_CTS_LOW: assert property (val_low_if_cts_low) 
-  else $error("User logic is sending data when TCP cts is low. Possible data lsoss.");
+  else $error("User logic is sending data when TCP cts is low. Possible data loss.");
   
 endmodule : tcp_vlg_tx_ctl_sva
